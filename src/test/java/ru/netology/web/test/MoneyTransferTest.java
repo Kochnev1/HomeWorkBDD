f710db6687a9387c2d3630845eb235aca3e1e289
Roman Kochnev <romaslog@icloud.com>
2023-10-10 22:17:10 +0500

    first steps

---
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -0,0 +1,25 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MoneyTransferTest {
+
+    @Test
+    void shouldMoneyTransferBetweenOwnCards() {
+        open("http://localhost:9999");
+
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+
+
+    }
+}
