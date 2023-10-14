57df60b839c6cbf2077ab6375c15172b346a11fb
Roman Kochnev <romaslog@icloud.com>
2023-10-14 20:27:07 +0500

    In process

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -2,6 +2,8 @@ package ru.netology.web.data;
 
 import lombok.Value;
 
+import java.util.Random;
+
 public class DataHelper {
     private DataHelper() {
     }
@@ -10,6 +12,12 @@ public class DataHelper {
     public static class AuthInfo {
         private String login;
         private String password;
+    }
+
+    @Value
+    public static class CardInfo {
+        private String cardNumber;
+        private String testId;
     }
 
     public static AuthInfo getAuthInfo() {
@@ -25,4 +33,23 @@ public class DataHelper {
         return new VerificationCode("12345");
 
     }
+
+    public static CardInfo getFirstCardInfo() {
+        return new CardInfo("5559 0000 0000 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
+    }
+
+    public static CardInfo getSecondtCardInfo() {
+        return new CardInfo("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+    }
+
+    public static int generateValidAmount(int balance) {
+        return new Random().nextInt(Math.abs(balance)) + 1;
+    }
+
+    public static int generateInvalidAmount(int balance) {
+        return Math.abs(balance) + new Random().nextInt(10000);
+
+    }
+
+
 }
